3d1e6654bcf323403bbbcf5955deca539904c41b
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2019-10-06 00:16:18 -0500

    Add ui element for testing, script to lower fill over time

---
+++ Assets/Scripts/PowerupTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerupTimer : MonoBehaviour
+{
+    [SerializeField]
+    private Image powerUpTimer;
+
+    private float amount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerUpTimer.GetComponent<Image>();
+
+        amount = 1;
+    }
+
+    private void Update()
+    {
+        if(Input.anyKey)
+        {
+            amount -= amount * Time.deltaTime;
+        }
+
+        powerUpTimer.fillAmount = amount;
+    }
+
+}
