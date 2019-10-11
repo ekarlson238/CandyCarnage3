fd6c004d2b52f4adf06a556f2fdbb8d5a29a57d3
ekarlson238 <ekarlson238@gmail.com>
2019-10-10 19:24:09 -0500

    fixing arrow update

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -13,7 +13,7 @@ public class Arrow : MonoBehaviour
     private float timePassed = 0;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         MoveForward();
         Despawn();
