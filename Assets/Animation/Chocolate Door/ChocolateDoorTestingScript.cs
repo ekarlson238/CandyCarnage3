3d7ea4665d6811b61d1ca4413b31cba617d3ddb9
Scott Little <scott.little@loop.colum.edu>
2019-11-19 01:00:28 -0600

    Created ChocolateDoubleDoor Prefab and Associated Animation Testing Script
    - Created my own scene named ScottsScene
    
    - Created a Chocolate Double Door asset composed of two Chocolate Door assets with the one on the right having an X scale value of -1 to invert in sideways and put the door handle on the opposite side.
    
    - Nested the two sides of the door to a parent object
    
    - Created a C# script that is only meant for testing the animations, however I am trying to make it so that it can be easily incorporated

---
+++ Assets/Animation/Chocolate Door/ChocolateDoorTestingScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Scott Little
+/// 
+/// Purpose:
+///     This script's sole purpose is to test the animations for the Chocolate Double Door Object.
+/// </summary>
+public class ChocolateDoorTestingScript : MonoBehaviour
+{
+    private Animator chocolateDoorAnimator;
+
+    [SerializeField]
+    private bool isOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chocolateDoorAnimator = GetComponent<Animator>();
+
+        // Set the animator's isOpen property to be equal to the default isOpen value. 
+        // This must use isOpen and NOT IsOpen since IsOpen isn't declared til after this function
+        chocolateDoorAnimator.SetBool("isOpen", isOpen);
+    }
+
+    /// <summary>
+    /// Used to make setting the door animator property isOpen easier to change.
+    /// 
+    /// **Note: Must come after Start() otherwise it will not know what animator component is being referenced.**
+    /// </summary>
+    public bool IsOpen
+    {
+        get {
+                isOpen = chocolateDoorAnimator.GetBool("isOpen");
+                return isOpen;
+            }
+        set { chocolateDoorAnimator.SetBool("isOpen", value); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsOpen == true)
+            chocolateDoorAnimator.SetBool("isOpen", IsOpen);
+        else if (IsOpen == false)
+            chocolateDoorAnimator.SetBool("isOpen", IsOpen);
+    }
+}
