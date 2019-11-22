9bf7bb1ac7817a4e090053bee42ebf11e466e336
Daniel Dababneh <32584314+DirtyDanChicago@users.noreply.github.com>
2019-11-22 11:50:01 -0600

    Add pause menu

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+
+    [Tooltip("Place the Pause panel here.")]
+    [SerializeField]
+    private GameObject pausePanel;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        PauseGame();
+    }
+
+
+    public void PauseGame()
+    {
+        if (Input.GetKey(KeyCode.Escape) && pausePanel.activeSelf == false)
+        {
+
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ResumeGame()
+    {
+            Time.timeScale = 1;
+
+            pausePanel.SetActive(false);
+    }
+
+}
